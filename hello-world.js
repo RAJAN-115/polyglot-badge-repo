92286bd5326547deb2bb2de46bdc489cfd66079a
RAJAN PRAJAPATI <rajanrp115@gmail.com>
2025-09-22 13:11:57 +0530

    Create hello-world.js

---
+++ hello-world.js
@@ -0,0 +1,15 @@
+// Simple JavaScript Hello World program
+console.log('Hello, World!');
+
+// Function to greet a user
+function greetUser(name) {
+    return `Hello, ${name}! Welcome to the polyglot repository.`;
+}
+
+// Example usage
+const userName = 'GitHub User';
+console.log(greetUser(userName));
+
+// Simple math operation
+const add = (a, b) => a + b;
+console.log('5 + 3 =', add(5, 3));
